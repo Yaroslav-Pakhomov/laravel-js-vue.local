b56dcc6af5cc9afc3f8b4200ae36ae340c765eca
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-05-07 08:30:27 +0500

    creating the first Vue component, parsing and changing the file app.js

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">11111</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
